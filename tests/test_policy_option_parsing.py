534d9bd9ad1522d5df1ff2535776d8f65b38b95f
andrewdscott12 <andrew@andrewdscott.com>
2026-04-15 10:21:53 -0400

    Tests didn't need to be in the repo

--- tests/test_policy_option_parsing.py
+++
@@ -1,9 +0,0 @@
-from sudo_to_ldif import parse_sudo_policy_line
-
-
-def test_standalone_ldap_option_is_not_treated_as_command() -> None:
-    parsed = parse_sudo_policy_line("ALL=(root) !requiretty")
-
-    assert parsed.runas_users == ("root",)
-    assert parsed.option_tokens == ("!requiretty",)
-    assert parsed.commands == tuple()
